f5d82cb96df67c1c4ad4de7ba54d9ffafc4665f9
Carlos Eduardo da Cruz Cunha <cruzcunha@MacBook-Air-de-Carlos.local>
2020-05-22 23:11:43 -0300

    config map

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 app = express();
 
+const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key';
+const UNITS = process.env.UNITS || 'metric';
+
 app.get('/', function (req, res) {
-  res.send('Hello World v2!\n');
+  res.send('Hello World v2: ${OWM_API_KEY},${UNITS}!\n');
 });
 
 app.get('/mars', function (req, res) {
